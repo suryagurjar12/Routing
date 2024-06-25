9d2987365e25a9dab2740098580af0997144012b
surya gurjar <suryagurjar781@gmail.com>
2024-06-25 18:30:04 +0530

    second commit

--- project/project/urls.py
+++ project/project/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from app import views as v1
 from app2 import views as v2
+from app3 import views as v3
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',v1.index,name="index"),
     path('home1/',v2.index1,name="index1"),
+    path('home2/',v3.index2,name="index2"),
 ]
